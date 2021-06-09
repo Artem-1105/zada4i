0e40abf0828b055dc16386b0b201a83de3ab9e36
Artem-1105 <smirnovaluda37@gmail.com>
2021-06-09 21:00:56 +0300

    фибоначи с циклом

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-const anagram = (str1, str2) => {
-    let arr1 = []
-    let arr2 = []
+const fibonacci = num => {
+
     let res = 0
-    str1 = str1.toLowerCase()
-    str2 = str2.toLowerCase()
+
+    let fibarr = [0,1]
+    if (num === 1) return 0
+    if (num === 2) return 1
+    for (let i=2; i<num; i++) {
+
+        res = fibarr[i-1] +fibarr[i-2]
+
+        fibarr.push(res)
+    }
     
-    for (let i=0; i<str1.length; i++) {
-        if (/[a-zа-я]/.test(str1[i])) arr1.push(str1[i])
-    }
-
-    for (let i=0; i<str2.length; i++) {
-        if (/[a-zа-я]/.test(str2[i])) arr2.push(str2[i])
-    }
-
-    if (arr1.length !== arr2.length || arr1.length === 0) return false
-
-    for (let i = 0; i<arr1.length; i++) {
-        for (let j = 0; j<arr2.length; j++) {
-            if (arr1[i] === arr2[j]) {
-                arr2[j] = ' '
-                arr1[i] = '*'
-                res = res + 1
-            }
-        }
-    }
-    return res === arr1.length && res === arr2.length ? true : false
+    return res
 }
-console.log (anagram('дд..о.....ОО..ДД...       ff', 'f          д>>>>ОFДДОО>>>>д'))
+console.log(fibonacci(4))
