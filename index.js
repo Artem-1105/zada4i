46489d4bc08c2133ca968592fdce513133e8904a
Artem-1105 <smirnovaluda37@gmail.com>
2021-06-09 17:04:38 +0300

    проверк анаграмм с циклами

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const FizzBuzz = (num) => {
-
-    for (let i=1; i<=num; i+=1) {
-
-       if (i % 3 === 0 && i % 5 === 0) console.log('fizzbuzz')
-       else if (i % 3 === 0) console.log('fizz')
-       else if (i % 5 === 0) console.log('buzz')
-       else console.log(i)
-    }
+const anagram = (str1, str2) => {
+    let arr1 = []
+    let arr2 = []
+    let res = 0
+    str1 = str1.toLowerCase()
+    str2 = str2.toLowerCase()
     
+    for (let i=0; i<str1.length; i++) {
+        if (/[a-zа-я]/.test(str1[i])) arr1.push(str1[i])
+    }
+
+    for (let i=0; i<str2.length; i++) {
+        if (/[a-zа-я]/.test(str2[i])) arr2.push(str2[i])
+    }
+
+    if (arr1.length !== arr2.length || arr1.length === 0) return false
+
+    for (let i = 0; i<arr1.length; i++) {
+        for (let j = 0; j<arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                arr2[j] = ' '
+                arr1[i] = '*'
+                res = res + 1
+            }
+        }
+    }
+    return res === arr1.length && res === arr2.length ? true : false
 }
-FizzBuzz(15)
+console.log (anagram('дд..о.....ОО..ДД...       ff', 'f          д>>>>ОFДДОО>>>>д'))
